c0a675d5fa4d83012efc3cdf40912b79e898d90c
Blair <bgriffit@oberlin.edu>
2019-05-13 15:29:29 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(width: 300, height: 400);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is best in life, Conan?");
+        }
+    }
+}
